d220c26873fcf6a486f47a412b2c512fa8d9f365
Berkeley Andrus <berkeley.andrus@gmail.com>
2019-11-11 21:10:55 -0700

    Got hints working better

--- berkeley/hint_giver.py
+++ berkeley/hint_giver.py
@@ -3,6 +3,7 @@ import random
 import time
 import io
 import numpy as np
+from scipy import spatial
 
 def load_vectors(fname):
     fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
@@ -27,8 +28,11 @@ class BaselineHintGiver:
             for j, card_word in enumerate(wordlist):
                 vocab_vector = self.vocab_vectors[vocab_word]
                 card_vector = self.card_vectors[card_word]
-                self.similarities[i][j] = np.dot(vocab_vector, card_vector)
+                # self.similarities[i][j] = np.dot(vocab_vector, card_vector)
+                self.similarities[i][j] = -spatial.distance.cosine(vocab_vector, card_vector)
+        self.std_similarity = np.std(self.similarities)
         print('Calculated all similarities')
+
 
     # Not actually good, just wanted to see how well it would behave. Turns out not very well.
     def give_hint_basic(self, my_indices, bad_indices, assassin_index):
@@ -43,6 +47,41 @@ class BaselineHintGiver:
             word_scores.append(score)
         return self.vocabulary[np.argmax(word_scores)]
 
+    def give_hint2(self, my_indices, bad_indices, assassin_index):
+        best_hint = ''
+        best_number = 0
+        for i, row in enumerate(self.similarities):
+            good_options = [row[index] for index in my_indices]
+            bad_options = [row[index] for index in bad_indices]
+            worst_option = row[assassin_index]
+
+            good_options.sort(reverse=True)
+            bad_options.sort(reverse=True)
+
+            benchmark = max(bad_options[0] + self.std_similarity, worst_option + 2 * self.std_similarity)
+
+            num = len([option for option in good_options if option > benchmark])
+
+            if num > best_number:
+                best_number = num
+                best_hint = self.vocabulary[i]
+                print(best_hint, best_number)
+
+        return best_hint, best_number
+
+    def give_hint(self, my_indices, bad_indices, assassin_index):
+        best_hint = ''
+        best_number = 0
+        for i, row in enumerate(self.similarities):
+            closest_words = np.argsort(-row)
+            for j, close_word in enumerate(closest_words):
+                if close_word not in my_indices:
+                    break
+            if j > best_number:
+                best_number = j
+                best_hint = self.vocabulary[i]
+                print(best_hint, best_number)
+        return best_hint, best_number
 
 
 def main():
@@ -60,12 +99,14 @@ def main():
 
     hintGiver = BaselineHintGiver(board)
 
-    hint = hintGiver.give_hint_basic(blue_indices, red_indices, assassin_index)
+    hint = hintGiver.give_hint(blue_indices, red_indices, assassin_index)
+    hint2 = hintGiver.give_hint2(blue_indices, red_indices, assassin_index)
 
     print('Want to guess:', [board[index] for index in blue_indices])
     print('Don\'t want to guess:', [board[index] for index in red_indices])
     print('Definitely don\'t want to guess:', board[assassin_index])
     print('Hint is', hint)
+    print('Other hint is', hint2)
 
 
     end = time.time()
