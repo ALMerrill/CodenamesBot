bc5f451d41d0acca411f7542b59994bc38c17f42
Berkeley Andrus <berkeley.andrus@gmail.com>
2019-12-10 01:09:14 -0700

    Tried (and failed) to overfit with distillation methods

--- berkeley/distillation.py
+++ berkeley/distillation.py
@@ -5,7 +5,18 @@ import numpy as np
 
 new_size = 299
 new_vectors_path = f'new-word-vectors/distillation-{new_size}.vec'
-learning_rate = 0.001
+learning_rate = 0.1
+learning_decay = 0.9999
+
+OVERFITTING = True
+if OVERFITTING:
+    new_vectors_path = f'new-word-vectors/overfit-distillation-{new_size}.vec'
+
+with open('shortened_wordlist.txt') as main_word_file:
+    main_words = [word.lower().strip() for word in main_word_file.readlines()]
+
+if OVERFITTING:
+    main_words = main_words[:25]
 
 new_word_vectors = {}
 
@@ -50,11 +61,26 @@ def report_progress(canonical_vectors, wip_vectors, vocabulary):
         error += abs(rov - wov)
     print(f'Error is {error}')
 
+def report_relevant_progress(canonical_vectors, wip_vectors, vocabulary):
+    test_words = np.random.choice(vocabulary, 10, replace=False)
+
+    error = 0
+    for word in main_words:
+        real_similarities = [np.dot(canonical_vectors[word], canonical_vectors[test_word]) for test_word in test_words]
+        wip_similarities = [np.dot(wip_vectors[word], wip_vectors[test_word]) for test_word in test_words]
+
+        real_order = np.argsort(real_similarities)
+        wip_order = np.argsort(wip_similarities)
+
+        for rov, wov in zip(real_order, wip_order):
+            error += abs(rov - wov)
+    print(f'Error is {error}')
+
 def train(canonical_vectors, wip_vectors, vocabulary):
     print('Training...')
     global new_word_vectors
     new_word_vectors = wip_vectors
-    iters = 500000
+    iters = 0
     while True:
         iters += 1
         words = np.random.choice(vocabulary, 3, replace=False)
@@ -89,6 +115,48 @@ def train(canonical_vectors, wip_vectors, vocabulary):
             report_progress(canonical_vectors, new_word_vectors, vocabulary)
 
 
+def train_overfit(canonical_vectors, wip_vectors, vocabulary):
+    print('Training...')
+    global new_word_vectors
+    new_word_vectors = wip_vectors
+    iters = 0
+    while True:
+        iters += 1
+        key_word = np.random.choice(main_words, 1)[0]
+        test_word = np.random.choice(vocabulary, 1)[0]
+        if test_word in main_words:
+            continue
+
+        real_sim = np.dot(canonical_vectors[key_word], canonical_vectors[test_word])
+        predicted_sim = np.dot(new_word_vectors[key_word], new_word_vectors[test_word])
+
+        loss = real_sim - predicted_sim
+
+        step = loss * learning_rate
+
+        # If step is postivie, move words[1] closer and words[2] away
+        # If step is negative, move words[2] closer and words[1] away
+
+        # print(f'Moving {test_word} {"closer to" if step > 0 else "farther from"} {key_word}')
+        # print(f'Key word: {new_word_vectors[key_word][:5]}')
+        # print(f'Test word: {new_word_vectors[test_word][:5]}')
+        for i in range(new_size):
+            dim_step = step * new_word_vectors[key_word][i]
+            # if i < 5:
+            #     print(f'Dim_step at {i}: {dim_step}')
+
+            new_word_vectors[test_word][i] += dim_step
+
+        # print(f'New test word: {new_word_vectors[test_word][:5]}')
+
+        # import pdb; pdb.set_trace()
+
+        if iters % 5000 == 0:
+            print(iters, end=' ')
+            report_relevant_progress(canonical_vectors, new_word_vectors, vocabulary)
+
+
+
 def main():
     signal.signal(signal.SIGINT, sigint_handler)
 
@@ -97,6 +165,8 @@ def main():
     with open('top_50k_cleaned.txt') as in_file:
         vocabulary = in_file.readlines()
     vocabulary = [word.strip().lower() for word in vocabulary]
+    if OVERFITTING:
+        vocabulary = list(set(vocabulary[:20] + main_words))
     canonical_vectors = {vocab_word: np.fromiter(canonical_data[vocab_word], dtype=float) for vocab_word in vocabulary}
 
     if path.exists(new_vectors_path):
@@ -105,7 +175,7 @@ def main():
     else:
         wip_vectors = rand_init_vectors(vocabulary)
 
-    train(canonical_vectors, wip_vectors, vocabulary)
+    train_overfit(canonical_vectors, wip_vectors, vocabulary)
 
 
 if __name__== "__main__":
